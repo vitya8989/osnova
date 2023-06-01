431b299b3f2500d58d1747cbf37f242228ec59a5
viktor <komarov.v@triadasite.ru>
2023-06-01 17:01:47 +0300

    fixes bugs

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -47,4 +47,25 @@ if (header) {
             });
         });
     }
+
+    const footer = document.querySelector('.footer');
+
+    if (footer) {
+        if (footer.querySelectorAll('a[href^="#"]').length > 0) {
+            footer.querySelectorAll('a[href^="#"]').forEach(link => {
+                link.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    let href = this.getAttribute('href').substring(1);
+                    const scrollTarget = document.getElementById(href);
+                    let topOffset = header.offsetHeight;
+                    const elementPosition = scrollTarget.getBoundingClientRect().top;
+                    const offsetPosition = elementPosition - topOffset;
+                    window.scrollBy({
+                        top: offsetPosition,
+                        behavior: 'smooth'
+                    });
+                });
+            });
+        }
+    }
 }
